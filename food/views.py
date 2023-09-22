105b08efe76af9e35519eadc4bdd9f47739ef74f
hanQ0Q <hanq01010@gmail.com>
2023-09-22 19:59:35 -0400

    user can select food and save them

--- food/views.py
+++ food/views.py
@@ -9,13 +9,28 @@ def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
 
+
     food_category = FoodCategory.objects.all()
+    user_foods = UserFoodItem.objects.filter(user=request.user)
     food_data = {}
     for category in food_category:
         food_data[category.name] = list(FoodItem.objects.filter(category=category).values_list("name"))
 
     print(f"food_data:  {food_data}")
-    context = {
-        'foods': food_data
+    data = {
+        'foods': food_data,
+        'user_foods': user_foods,
     }
-    return render(request, "food/food.html", context=context)
+    return render(request, "food/food.html", context=data)
+
+def add(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    if request.method == "POST":
+        selected_item = request.POST.get('selectedItem')
+        item = FoodItem.objects.get(name=selected_item)
+        if UserFoodItem.objects.filter(item=item, user=request.user).exists():
+            return HttpResponseRedirect(reverse("food:index"))
+        user_food = UserFoodItem.objects.create(item=item, user=request.user)
+        user_food.save()
+        return HttpResponseRedirect(reverse("food:index"))
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
     return render(request, "recipes/recipes.html")
