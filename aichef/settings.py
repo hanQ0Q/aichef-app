5c053fcf6a66346fc368063ca015d6e05e4c0c11
hanQ0Q <hanq01010@gmail.com>
2023-09-23 16:52:58 -0400

    add staitc ip to allowed_hosts

--- aichef/settings.py
+++ aichef/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['3.15.149.223']
+ALLOWED_HOSTS = ['18.223.73.115']
 
 
 # Application definition
